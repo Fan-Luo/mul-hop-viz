8c40f249acae767c97500f96ca6e95ec661f9e77
Fan-Luo <luofan813@gmail.com>
2019-02-16 18:06:58 -0700

    tsne moved to falsk server, providing 2-d embedding to html for rendering, fix zoom

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
 from flask import render_template
+from flask.json import jsonify
 import string, random
+from sklearn.manifold import TSNE
+import json
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,29 +14,42 @@ def user():
   user_dict = {'first': 'Robert', 'last': 'Chang', 'twitter_handle': '@_rchang'}
   return render_template("user.html", user_dict = user_dict.values())
   
-@app.route("/data")
+# @app.route("/data")
 
-def data():
-	num_facts = request.args.get('text')
-	text = loadfacts(int(num_facts))
+# def data():
+#     num_facts = request.args.get('text')
+#     text = loadfacts(int(num_facts))
 
-	# print(int(num_facts))
-	# print(text)
+#     # print(int(num_facts))
+#     # print(text)
 
-	return render_template("user.html", text = text)
-
-
-def read_file(datafile):
-    with open(datafile, 'r') as f:
-	    lines = [line for line in f]
-	    return lines
+#     return render_template("user.html", text = text)
 
 
-def loadfacts(n):
-	facts = read_file('openbook.txt')
+# def read_file(datafile):
+#     with open(datafile, 'r') as f:
+#         lines = [line for line in f]
+#         return lines
 
-	f = []
-	for i in range(int(n)):
-		f_id = random.randint(0, len(facts))
-		f.append(facts[f_id])
-	return f
+
+# def loadfacts(n):
+#     facts = read_file('openbook.txt')
+
+#     f = []
+#     for i in range(int(n)):
+#         f_id = random.randint(0, len(facts))
+#         f.append(facts[f_id])
+#     return f
+
+
+
+@app.route("/tsne")
+def tsne():
+    with open('static/knowledge_fact_fan_49.json') as json_file:  
+        data = json.load(json_file)
+        X_embedded = TSNE(n_components=2, perplexity=40, n_iter=300).fit_transform(data['mat'])
+
+    return render_template("user.html", X_embedded = X_embedded.tolist())
+    #return render_template("user.html", X_embedded = np.array2string(X_embedded, separator=', '))
+
+ 
