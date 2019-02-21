d08503e408552b93353def2a592b7debae3f362c
Fan-Luo <luofan813@gmail.com>
2019-02-20 22:59:53 -0700

    clear code

--- multi-hop-reasoning.py
+++ multi-hop-reasoning.py
@@ -10,39 +10,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def user():
-  # title = 'Visualization for Multi-hop Reasoning'
-  user_dict = {'first': 'Robert', 'last': 'Chang', 'twitter_handle': '@_rchang'}
+  user_dict = {'first': 'Fan', 'last': 'Luo'}
   return render_template("user.html", user_dict = user_dict.values())
   
-# @app.route("/data")
-
-# def data():
-#     num_facts = request.args.get('text')
-#     text = loadfacts(int(num_facts))
-
-#     # print(int(num_facts))
-#     # print(text)
-
-#     return render_template("user.html", text = text)
-
-
-# def read_file(datafile):
-#     with open(datafile, 'r') as f:
-#         lines = [line for line in f]
-#         return lines
-
-
-# def loadfacts(n):
-#     facts = read_file('openbook.txt')
-
-#     f = []
-#     for i in range(int(n)):
-#         f_id = random.randint(0, len(facts))
-#         f.append(facts[f_id])
-#     return f
-
-
-
 @app.route("/tsne")
 def tsne():
     with open('static/knowledge_fact_fan_49.json') as json_file:  
