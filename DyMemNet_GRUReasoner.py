851a66251564904320834ac500a3072bc0055c11
Zhengzhong Liang <zhengzhongliang93@gmail.com>
2019-04-09 16:19:32 -0700

    fix annotation

--- DyMemNet_GRUReasoner.py
+++ DyMemNet_GRUReasoner.py
@@ -34,12 +34,6 @@ dict_path = "/lhome/zhengzhongliang/CLU_Projects/glove.840B.300d.pickle"
 with open(dict_path, 'rb') as input_file:
     glove_dict = pickle.load(input_file)
 
-class Annotation():
-    def __init__(self, question_id, annotations, new_facts):
-        self.question_id = question_id
-        self.annotations = annotations
-        self.new_facts = new_facts
-
 class InputModule(nn.Module):
     def __init__(self, hidden_size=100):
         super(InputModule,self).__init__()
--- saved_annotations/1_check_pickle.py
+++ saved_annotations/1_check_pickle.py
@@ -1,8 +1,21 @@
 import pickle
 
+class Instance:
+    def __init__(self, target,  question_text, choices_text, fact_text, knowledge_text):
+        self.question_text = question_text
+        self.choices_text = choices_text
+        self.science_fact_text = fact_text
+        self.knowledge_fact_text = knowledge_text
+        self.target = target
+        self.question_id = 0
 
+class Annotation():
+    def __init__(self, question_id, annotations, new_facts):
+        self.question_id = question_id
+        self.annotations = annotations
+        self.new_facts = new_facts
 
-with open('zhengzhongliang_2019-04-09_1603.pickle', 'rb') as f:
+with open('zhengzhongliang_2019-04-09_1618.pickle', 'rb') as f:
     pickle_file = pickle.load(f)
     print(pickle_file)
     pickle_file = pickle.load(f)
